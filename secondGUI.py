ef84f7aa51c7e9e1f72bc66aec0a6951bcef1da9
sigmarion1 <axsim@naver.com>
2018-12-24 19:12:12 +0900

    apply qt designer ui

--- secondGUI.py
+++ secondGUI.py
@@ -1,24 +1,17 @@
 import sys
-import PyQt5
 
+import PyQt5
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-# from PyQt5 import uic
+from PyQt5 import uic
 
-# CalUI = 'calculator.ui'
+CalUI = 'calculator.ui'
 
 class MainDialog(QDialog):
     def __init__(self):
         QDialog.__init__(self, None)
-
-        self.setFixedSize(300, 200)
-        self.lineEdit = QLineEdit(self)
-        self.pushButton = QPushButton(self)
-        self.pushButton.move(0,100)
-
-
-
+        uic.loadUi(CalUI, self)
 
 
 app = QApplication(sys.argv)
