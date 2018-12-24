37263695fe1c1ba54ccafc9c9699bdc5c0e66c05
sigmarion1 <axsim@naver.com>
2018-12-24 18:54:18 +0900

    add push button and line input

--- firstGUI.py
+++ firstGUI.py
@@ -9,6 +9,14 @@ class MainDialog(QDialog):
     def __init__(self):
         QDialog.__init__(self, None)
 
+        self.setFixedSize(300, 200)
+        self.lineEdit = QLineEdit(self)
+        self.pushButton = QPushButton(self)
+        self.pushButton.move(0,100)
+
+
+
+
 
 app = QApplication(sys.argv)
 main_dialog = MainDialog()
