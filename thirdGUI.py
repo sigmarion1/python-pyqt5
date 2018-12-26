dc97c4fa1631354e202080b19b0da72160047842
sigmarion1 <axsim@naver.com>
2018-12-27 01:51:07 +0900

    add image

--- thirdGUI.py
+++ thirdGUI.py
@@ -1,12 +1,13 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QComboBox)
+from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QComboBox, QPushButton)
+from PyQt5.QtGui import QPixmap
 # from PyQt5.QtGui import QIcon
 
 class App(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 window'
+        self.title = '학생 자리 배치'
         self.left = 100
         self.top = 100
         self.width = 640
@@ -14,20 +15,25 @@ class App(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.lb1 = QLabel(self)
+        self.pixmap = QPixmap('classroom.png')
+        self.lb1.setPixmap(self.pixmap)
+        self.resize(self.pixmap.width(),self.pixmap.height())
 
-
-        self.lb1 = QLabel('Option1', self)
-
+        self.lb2 = QLabel("남학생 수는?", self)
+        self.btn1 = QPushButton("입력", self)
 
         grid = QGridLayout()
         self.setLayout(grid)
 
         grid.addWidget(self.lb1, 0, 0)
+        grid.addWidget(self.lb2, 0, 1)
+        grid.addWidget(self.btn1, 0, 0)
+
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.show(self)
 
-
+        self.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
